6625f6ecbbe881feb23bf80519c3961dd2ad6639
Vadim Semenov <vsemenov@devexperts.com>
2017-08-19 15:09:21 +0300

    [I Introduction] Complete first chapter

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,8 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(fst: Int, snd: Int): Int = snd - fst
+    })
     return arrayList
 }
